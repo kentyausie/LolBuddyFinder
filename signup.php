44d72a7f83d75de5584b023e2630c1fc48f5ad6e
Kent <jediknight101@gmail.com>
2015-03-23 01:55:18 -0600

    Changes

--- signup.php
+++ signup.php
@@ -24,6 +24,10 @@
     font-size:25px;
 }
 .selectBox2 {
+	padding-top: 100px;
+    padding-right: 100px;
+    padding-bottom: 100px;
+    padding-left: 100px;
 	width: 300px;
 	height: 34px;
 	overflow: hidden;
