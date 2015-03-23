8d2ce6b5ab88fa3b98e9a0ffd15fda71d195d1bc
Kent <jediknight101@gmail.com>
2015-03-23 01:10:42 -0600

    Changes

--- signup.php
+++ signup.php
@@ -1,4 +1,3 @@
-<?php include 'validation.php';?>
 <!DOCTYPE HTML>
 <html>
 <style>
@@ -21,28 +20,21 @@
 <img src="headpic.jpg" class="headImg"
 <title>Sign up form</title>
 </head>
+
 <body>
-<div class="container">
-<div class="main">
 <h2>Sign up form</h2>
-<p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <p class="box">
-<input class="textbox" type="text" name="sumName" placeholder="Summoner Name" value="">
-<span class="error">*<?php echo $sumError;?></span><br>
+<input class="textbox" type="text" name="sumName" placeholder="Summoner Name" value=""><br>
 
-<input class="textbox" type="email" name="email" placeholder="Email" value="">
-<span class="error">*<?php echo $lnameError;?></span><br>
+<input class="textbox" type="email" name="email" placeholder="Email" value=""><br>
 
-<input class="textbox" type="password" name="password1" placeholder="Password" value="">
-<span class="error">*<?php echo $companyError;?></span><br>
+<input class="textbox" type="password" name="password1" placeholder="Password" value=""><br>
 
-<input class="textbox" type="password" name="password2" placeholder="Re-type Password" value="">
-<span class="error">*<?php echo $phoneError;?></span><br>
+<input class="textbox" type="password" name="password2" placeholder="Re-type Password" value=""><br>
 
 </p>
 <input class="textbox" type="submit" name="submit" value="Submit">
-<span class="success"><?php echo $successMessage;?></span><br>
 </form>
 
 <form action="signup.php">
