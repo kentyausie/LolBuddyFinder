d2285d3882807ad352683f6c57b99ea22b77968d
Kent <jediknight101@gmail.com>
2015-03-22 22:57:20 -0600

    Changes

--- signup.php
+++ signup.php
@@ -20,20 +20,20 @@ border-width: medium;
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <p class="box">
 <input class="input" type="text" name="sumName" placeholder="Summoner Name" value="">
-//<span class="error">*<?php echo $fnameError;?></span><br>
+<span class="error">*<?php echo $fnameError;?></span><br>
 
-<input class="email" type="email" name="email" placeholder="email" value="">
-//<span class="error">*<?php echo $lnameError;?></span><br>
+<input class="email" type="email" name="email" placeholder="Email" value="">
+<span class="error">*<?php echo $lnameError;?></span><br>
 
 <input class="input" type="password" name="password1" placeholder="Password" value="">
-//<span class="error"><?php echo $companyError;?></span><br>
+<span class="error"><?php echo $companyError;?></span><br>
 
 <input class="input" type="password" name="password2" placeholder="Re-type Password" value="">
-//<span class="error">*<?php echo $phoneError;?></span><br>
+<span class="error">*<?php echo $phoneError;?></span><br>
 
 </p>
 <input class="submit" type="submit" name="submit" value="Submit">
-//<span class="success"><?php echo $successMessage;?></span><br>
+<span class="success"><?php echo $successMessage;?></span><br>
 </form>
 
 <form action="signup.php">
