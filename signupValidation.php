92e59d0c003acd354a14f8f5512acd24d0dcbd87
Kent <jediknight101@gmail.com>
2015-04-16 21:50:35 -0600

    Changes

--- signupValidation.php
+++ signupValidation.php
@@ -31,9 +31,7 @@ if(isset($_POST['submit'])) { // Checking null values in message.
 		$valid = "false"; 
 	} else {
 		$sumName = test_input($_POST["sumName"]); // check name only contains letters and whitespace
-		echo '<script language="javascript" src="myScripts.js">';
-		echo 'summonerLookUp();';
-		echo '</script>';
+		echo '<script language="javascript" src="myScripts.js">summonerLookUp()</script>';
 	} // Checking null values in the message.
 
 	if (empty($_POST["email"])){
