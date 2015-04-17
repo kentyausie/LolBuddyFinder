c536f611ef418ebc0fa2f3c7da2d805a8e9edfa3
Kent <jediknight101@gmail.com>
2015-04-17 03:43:14 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -390,7 +390,7 @@ function summonerLookUp() {
 	}
 	
 	
-	if(!region === ""){
+	if(region.localeCompare("") !=0){
 		alert("Hello2");
     	var sumName = document.getElementById("sumName").value;
     	var API_KEY = "01edb1d0-a26b-4f78-afbb-3eeb9de5b0f9";
