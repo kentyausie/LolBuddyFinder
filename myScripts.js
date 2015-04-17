e89b59906c8fa48fb61c06adf110babbef40e85e
Kent <jediknight101@gmail.com>
2015-04-17 03:26:51 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -383,13 +383,13 @@ function KeepCount() {
 
 function summonerLookUp() {
 	
-	alert("Hello");
+	alert(document.getElementById("region").checked);
 	if(empty(document.getElementById("region"))){
 		
 		alert("Select your region");
 	} else {
 		alert("Hello2");
-		var region = document.getElementById("region");
+		var region = document.getElementById("region").value;
     	var sumName = document.getElementById("sumName").value;
     	var API_KEY = "01edb1d0-a26b-4f78-afbb-3eeb9de5b0f9";
     	if (sumName !== "") {
