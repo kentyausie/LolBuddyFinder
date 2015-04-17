22d60b4745d8aa4040fa6a67513059ca79e89526
Kent <jediknight101@gmail.com>
2015-04-17 03:53:16 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -431,7 +431,6 @@ function summonerLookUp() {
 			if(xmlhttp.status == 200){
 				var arr = JSON.parse(xmlhttp.responseText);
 				var sumInfo = arr[sumName].id;
-				alert(sumInfo);
 				document.getElementById("sumInfo").value = sumInfo;
 			}
 			else if(xmlhttp.status == 404){
