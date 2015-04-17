f9fae2e1368bf118b5ffc2ef17273912c86fbfa0
Kent <jediknight101@gmail.com>
2015-04-17 01:17:47 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -387,7 +387,7 @@ function summonerLookUp() {
     var API_KEY = "01edb1d0-a26b-4f78-afbb-3eeb9de5b0f9";
 
     if (sumName !== "") {
-		
+		alert("Hello");
         var xmlhttp = new XMLHttpRequest();
 		var url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+sumName+"?api_key="+API_KEY;
 
@@ -395,6 +395,7 @@ function summonerLookUp() {
 		xmlhttp.open("GET", url, false);
 		xmlhttp.send();
 		if(xmlhttp.status == 200){
+			alert("200");
 			var arr = JSON.parse(xmlhttp.responseText);
 			var sumInfo = arr[sumName].id;
 			alert(""+sumInfo);
