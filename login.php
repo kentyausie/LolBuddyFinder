9629d9e5bc3e88d9a697d6fed9aa36472b4ba286
Kent <jediknight101@gmail.com>
2015-04-18 17:14:45 -0600

    Changes

--- login.php
+++ login.php
@@ -42,11 +42,12 @@
     		echo $conn->error;
     		alert("database fail");
 		}
-
+		echo "<script> alert('hello1'); </script>";
 		$result = $conn->query($sql);
-
+		echo "<script> alert('hello2'); </script>";
 		if ($result->num_rows > 0) {
     		$row = $result->fetch_assoc();
+    		echo "<script> alert('hello3'); </script>";
 		} else {
 			$emailError = "Invalid email";
 			$valid = "false";
