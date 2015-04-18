e251c03eb20c300e3ec9774926a418ce25ff4891
Kent <jediknight101@gmail.com>
2015-04-18 16:59:09 -0600

    Changes

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 	
 </head>
 <body>
-		<form name="login" style="text-align:center;" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<form name="login" style="text-align:center;" method="post" action="index.php">
 			<input class="textbox" type="email" value="" placeholder="E-Mail Address" name="email"></input>
 			<span class="error"><?php echo $emailError;?></span><br>
-			<input class="textbox" type="password" value="" placeholder="Password" name="password"><br><br>
-			<span class="error"><?php echo $passwordError;?></span><br>
+			<input class="textbox" type="password" value="" placeholder="Password" name="password">
+			<span class="error"><?php echo $passwordError;?></span><br><br>
 			<input type="submit" class="submitButton" name="submit" value="Log In">
 		</form>
 		<form action="signup.php" style="text-align:center;padding-top: 15px;">
