6f243f3fc5e1929ecd5d7c7987490a0cc39da0cc
Kent <jediknight101@gmail.com>
2015-04-18 23:04:22 -0600

    Changes

--- profile.php
+++ profile.php
@@ -53,6 +53,7 @@ $conn->close();
 			else{
 				alert("Unknown error");
 			}
+			alert(sumInfo.profileIconId);
     	}
     	
     	var id = sumInfo.id;
@@ -101,7 +102,7 @@ $conn->close();
 <script>
 	document.getElementById('name').innerHTML = ""+sumInfo.name;
 	document.getElementById('level').innerHTML = ""+sumInfo.summonerLevel;
-	//document.getElementById('id').innerHTML = ""+sumInfo.id;
+	document.getElementById('id').innerHTML = ""+id;
 	document.getElementById('rank').innerHTML = ""+leagueInfo[0].tier;
 	document.getElementById("profileIcon").src = "http://ddragon.leagueoflegends.com/cdn/4.3.12/img/profileicon/"+sumInfo.profileIconId+".png";
 </script>
