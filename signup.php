f9bce783b650bb35747379d7910e582d70a4e1ae
eddie81020 <eddie81020@gmail.com>
2015-04-18 19:16:28 -0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -394,10 +394,19 @@
 <select name="language" class="selectBox">
 <option value="english">English</option>
 <option value="french">French</option>
-<option value="spanish">French</option>
-<option value="korean">Korean</option>
-<option value="chinese">Chinese</option>
-<option value="japanese">Japanese</option>
+<option value="german">German</option>
+<option value="spanish">Spanish</option>
+<option value="portuguese">Portuguese</option>
+<option value="korean">Korean 한국어</option>
+<option value="mandarin">Mandarin 中文/国语</option>
+<option value="cantonese">Cantonese 廣東話</option>
+<option value="japanese">Japanese 日本語</option>
+<option value="tagalog">Tagalog</option>
+<option value="vietnamese">Vietnamese</option>
+<option value="arabic">Arabic</option>
+<option value="punjabi">Punjabi</option>
+<option value="hindi">Hindi</option>
+
 <option value="penguin">Penguin</option>
 </select><br><br>
 <input class="submitButton" type="submit" name="submit" value="Submit">
