8cae4c06a230f42385f9dbf46614dbc3d0babbbf
Kent <jediknight101@gmail.com>
2015-04-21 03:13:02 -0600

    Changes

--- signup.php
+++ signup.php
@@ -126,7 +126,7 @@ function summonerLookUp() {
 
 <label class="formDescriptionText">Summoner Name:&nbsp&nbsp</label>
 
-<input class="textbox" type="text" name="sumName" id="sumName" onblur="summonerLookUp()" placeholder="Summoner Name" value="">
+<input class="textbox" type="text" name="sumName" id="sumName" onblur="summonerLookUp" placeholder="Summoner Name" value="">
 <span class="error">*<?php echo $sumError;?></span><br>
 
 <input type="hidden" name="sumInfo" id="sumInfo" value="">
