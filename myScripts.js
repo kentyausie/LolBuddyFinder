746419b313b71903847818c0bc603e9fbbe4039a
Kent <jediknight101@gmail.com>
2015-04-21 04:16:12 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -437,7 +437,7 @@ function summonerLookUp() {
 				alert("Hello2");
 				var sumInfo = arr[sumName];
 				alert("Hello3");
-				alert(sumInfo);
+				alert(sumInfo.id);
 				alert("Hello4");
 				document.getElementById("sumInfo").value = sumInfo;
 				document.getElementById("sumRegion").value = region;
