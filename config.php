cc53f5f709e4277c45371877a478259d98b090c4
eddie81020 <eddie81020@gmail.com>
2015-04-21 22:19:47 -0600

    Update config.php

--- config.php
+++ config.php
@@ -10,4 +10,19 @@
 		die("Connection failed: " . $conn->connect_error);
 		echo "<script> alert('connction failure');";
 	}
+	
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
 ?>
