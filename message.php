06e150f29ed06620676248a03eef36ce3de49ca6
eddie81020 <eddie81020@gmail.com>
2015-04-22 07:07:45 -0600

    Update message.php

--- message.php
+++ message.php
@@ -12,44 +12,15 @@
  $pm = new cpm($userid);
   
  function sendmail($to,$subject = '',$body = ''){
-error_reporting(E_STRICT);
-
-date_default_timezone_set('America/Toronto');
-
-require_once('../class.phpmailer.php');
-//include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
-
-$mail             = new PHPMailer();
-
-$body             = file_get_contents('contents.html');
-$body             = eregi_replace("[\]",'',$body);
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->SMTPDebug  = 2;                      // enables SMTP debug information (for testing)
-                                            // 1 = errors and messages
-                                            // 2 = messages only
-                                            echo "test1";
-$mail->SMTPAuth   = "true"                   // enable SMTP authentication
-$mail->SMTPSecure = "tls";                  // sets the prefix to the servier
-$mail->Host       = "smtp.live.com";        // sets hotmil as the SMTP server
-$mail->Port       = 587;                    // set the SMTP port for the hotmail server
-$mail->Username   = "lolteambuilder@hotmail.com";      // hotmail username
-$mail->Password   = "P@$$word123";           // hotmail password
-echo "tes2";
-$mail->SetFrom('lolteambuilder@hotmail.com', 'First Last');
-$mail->AddReplyTo('lolteambuilder@hotmail.com',"First Last");
-$mail->Subject    = "PHPMailer Test Subject via smtp (hotmail), basic";
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-$mail->MsgHTML($body);
-
-$address = "eddie81020@gmail.com";
-$mail->AddAddress($address, "John Doe");
-echo "test";
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
-}
+  $mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host       = "smtp.gmail.com";      // SMTP server example, use smtp.live.com for Hotmail
+$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 465;                   // SMTP port for the GMAIL server 465 or 587
+$mail->Username   = "eddie81020@gmail.com";  // SMTP account username example
+$mail->Password   = "jiz322tif192";            // SMTP account password example
 
 }
   
