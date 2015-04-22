e8bea1e1656cbc02c533a83bebd75e02ea80183b
eddie81020 <eddie81020@gmail.com>
2015-04-22 07:04:59 -0600

    Update message.php

--- message.php
+++ message.php
@@ -31,10 +31,10 @@ $mail->SMTPDebug  = 2;                      // enables SMTP debug information (f
                                             echo "test1";
 $mail->SMTPAuth   = "true"                   // enable SMTP authentication
 $mail->SMTPSecure = "tls";                  // sets the prefix to the servier
-$mail->Host       = 'smtp.live.com';        // sets hotmil as the SMTP server
+$mail->Host       = "smtp.live.com";        // sets hotmil as the SMTP server
 $mail->Port       = 587;                    // set the SMTP port for the hotmail server
-$mail->Username   = 'lolteambuilder@hotmail.com';      // hotmail username
-$mail->Password   = 'P@$$word123';           // hotmail password
+$mail->Username   = "lolteambuilder@hotmail.com";      // hotmail username
+$mail->Password   = "P@$$word123";           // hotmail password
 echo "tes2";
 $mail->SetFrom('lolteambuilder@hotmail.com', 'First Last');
 $mail->AddReplyTo('lolteambuilder@hotmail.com',"First Last");
