4b674195186e6fc96ffd27b684aa7e547a7485e9
Kent <jediknight101@gmail.com>
2015-04-22 06:28:13 -0600

    Changes

--- myScripts.js
+++ myScripts.js
@@ -445,7 +445,7 @@ function summonerLookUp() {
 				xmlhttp.send();
 				if(xmlhttp.status == 200){
 					arr = JSON.parse(xmlhttp.responseText);
-					var sumLeague = arr[sumId]['tier'];
+					var sumLeague = arr[sumId][0]['tier'];
 					alert(sumLeague);
 					document.getElementById("tier").value = sumLeague;
 					alert("League");
