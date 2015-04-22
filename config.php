3f23e94593dd66497b465ca984613418f93d83f8
eddie81020 <eddie81020@gmail.com>
2015-04-21 23:05:39 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
-<?php 
-	$servername = "lovett.usask.ca";
-	$username = "cmpt350_key053"; 
-	$password = "awp0t6pokb";
-	$dbname = "cmpt350_key053";
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-		echo "<script> alert('connction failure');";
-	}
-	
+<?php
+$servername = "lovett.usask.ca";
+$username = "cmpt350_key053"; 
+$password = "awp0t6pokb";
+$dbname = "cmpt350_key053";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    echo "<script> alert('connction failure');";
+} 
+echo "Connected successfully";
 ?>
