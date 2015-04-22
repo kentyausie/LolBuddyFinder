3d9b0c21c90d2a5d3488302a3f278593c539084a
Kent <jediknight101@gmail.com>
2015-04-22 11:20:51 -0600

    Changes

--- profile.php
+++ profile.php
@@ -81,12 +81,6 @@ var sumName = document.getElementById("sumName").value;
 	<form action="logout.php" style="text-align:center;padding-top: 15px;display: inline;">
 		<input type="submit" name="submit" value="Logout">
 	</form>
-	<form action="config.php" style="text-align:center;padding-top: 15px;display: inline;">
-		<input type="submit" name="submit" value="Config">
-	</form>
-	<form action="pm.php" style="text-align:center;padding-top: 15px;display: inline;">
-		<input type="submit" name="submit" value="PM">
-	</form>
 	<form action="message.php" style="text-align:center;padding-top: 15px;display: inline;">
 		<input type="submit" name="submit" value="Message">
 	</form>
