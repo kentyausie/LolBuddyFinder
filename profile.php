44eb15ce38e28e83c2a08b2a1ebfb0f7320ba20c
Kent <jediknight101@gmail.com>
2015-04-21 18:24:10 -0600

    Changes

--- profile.php
+++ profile.php
@@ -63,7 +63,7 @@ if ($result->num_rows > 0) {
 			xmlhttp.open("GET", url, false);
 			xmlhttp.send();
 			if(xmlhttp.status == 200){
-				var arr2 = JSON.Parse(xmlhttp.responseText);
+				var arr2 = JSON.parse(xmlhttp.responseText);
 				var leagueInfo = arr2[sumId];
 			}
 			else if(xmlhttp.status == 404){
