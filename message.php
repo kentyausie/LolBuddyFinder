d9b22794e1810f02070fba1ce941f07b97eeaa2e
eddie81020 <eddie81020@gmail.com>
2015-04-22 05:09:37 -0600

    Update message.php

--- message.php
+++ message.php
@@ -18,12 +18,19 @@
      // Tell the user it was successful
      echo "Message successfully sent!";
      
-$to = "eddie81020@gmail.com";
-$subject = "New Message from lolbuddyfinder";
-$txt = "You've got mail!";
-$headers = "From: lolteambuilder@hotmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-mail($to,$subject,$txt,$headers);
+
+//define the receiver of the email
+$to = 'eddie81020@gmail.com';
+//define the subject of the email
+$subject = 'Test email'; 
+//define the message to be sent. Each line should be separated with \n
+$message = "Hello World!\n\nThis is my first mail."; 
+//define the headers we want passed. Note that they are separated with \r\n
+$headers = "From: lolteambuilder@hotmail.com\r\nReply-To: lolteambuilder@hotmail.com";
+//send the email
+$mail_sent = @mail( $to, $subject, $message, $headers );
+//if the message is sent successfully print "Mail sent". Otherwise print "Mail failed" 
+echo $mail_sent ? "Mail sent" : "Mail failed";
 
    } else {
      // Tell user something went wrong it the return was false
