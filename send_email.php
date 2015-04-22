cfd87d7eeeb954e8fc4e732f03a788904cd31b0d
eddie81020 <eddie81020@gmail.com>
2015-04-22 10:12:45 -0600

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,19 +1,17 @@
 <?php
-require_once 'lib/swift_required.php';
- 
-// Create the mail transport configuration
-$transport = Swift_MailTransport::newInstance('smtp.live.com', 25);
- 
-// Create the message
-$message = Swift_Message::newInstance();
-$message->setTo(array(
-  "eddie81020@gmail.com" => "Eddie",
-  "eddie81020@gmail.com" => "eddie"
-));
-$message->setSubject("This email is sent using Swift Mailer");
-$message->setBody("You're our best client ever.");
-$message->setFrom("lolteamfinder@hotmail.com", "ABC");
- 
-// Send the email
+require_once 'Swift/lib/swift_required.php';
+
+$transport = Swift_SmtpTransport::newInstance('smtp.live.com', 25)
+  ->setUsername('lolteambuildere@hotmail.com')
+  ->setPassword('P@$$word123');
+
 $mailer = Swift_Mailer::newInstance($transport);
-$mailer->send($message);
+$message = Swift_Message::newInstance('Wonderful Subject')
+  ->setFrom(array('lolteambuilder@hotmail.com' => 'MY NAME'))
+  ->setTo(array('eddie81020@gmail.com' => 'YOU'))
+  ->setBody('This is the text of the mail send by Swift using SMTP transport.');
+//$attachment = Swift_Attachment::newInstance(file_get_contents('path/logo.png'), 'logo.png');  
+//$message->attach($attachment);
+$numSent = $mailer->send($message);
+printf("Sent %d messages\n", $numSent);
+?>
