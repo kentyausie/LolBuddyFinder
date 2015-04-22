9d0cef933e2967e5e5f8fa505823e6011bd9a3c2
eddie81020 <eddie81020@gmail.com>
2015-04-22 07:38:19 -0600

    Update message.php

--- message.php
+++ message.php
@@ -11,47 +11,7 @@
  // initiate a new pm class
  $pm = new cpm($userid);
   
- function sendmail($to,$subject = '',$body = ''){
-    //Author:Codemug Website: http://codemug.com
-    //$to:receiver's email address; $subject:title of the mail; $body: content of the mail
-    //error_reporting(E_ALL);
-    error_reporting(E_STRICT);
-    date_default_timezone_set('America/Los_Angeles');//Set the server's timezone
-    require_once('class.phpmailer.php');
-    include('class.smtp.php');
-    
-    echo "STEP1";
-    
-    $mail             = new PHPMailer(); //Initialize a new PHPMailer object;
-    $body            = eregi_replace("[\]",'',$body); //Replace unwanted characters of the content
-    $mail->CharSet ="ISO-8859-1";//Set the character set you need to specify
-    $mail->IsSMTP(); // Use SMTP service
-    $mail->SMTPDebug  = 1;                     // Enable debugging for SMTP
-    // 1 = errors and messages
-    // 2 = messages only
-    $mail->SMTPAuth   = true;                  // Enable authentication feature for the SMTP server
-    //$mail->SMTPSecure = "TLS";                 // Use SSL, you may comment this line out
-    $mail->Host       = 'smtp.live.com';      // SMTP server
-    $mail->Port       = 25;                   //SMTP port, not all email services use default port 25, please refer to your mail service provider.
-    $mail->Username   = 'lolteambuilder@hotmail.com';  //Username of your email account
-    $mail->Password   = 'P@$$word123';            //Password of your email account
-    $mail->SetFrom('lolteambuilder@hotmail.com', 'who');
-    $mail->AddReplyTo('lolteambuilder@hotmail.com','who');
-    $mail->Subject    = $subject;
-    $mail->AltBody    = 'To view the message, please use an HTML compatible email viewer!'; // optional, comment out and test
-    $mail->MsgHTML($body);
-    $address = $to;
-    echo "STEP2";
-    $mail->AddAddress($address, '');
-    //$mail->AddAttachment("images/phpmailer.gif");      // attachment
-    //$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-    echo "STEP3";
-    if(!$mail->Send()) {
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-//        echo "Message sent successfully!";
-    }
-}
+
   
  // check if a new message had been send
  if(isset($_POST['newmessage'])) {
@@ -60,7 +20,6 @@
   
      echo "Message successfully sent!";
 
-     sendmail('eddie81020@gmail.com','Howdy!','Hello, my friend!');
 
    } else {
      // Tell user something went wrong it the return was false
