22473ba2e0a77e9d577c004075011b5ea6fc609b
eddie81020 <eddie81020@gmail.com>
2015-04-22 09:19:59 -0600

    Update message.php

--- message.php
+++ message.php
@@ -19,7 +19,41 @@
    if($pm->sendmessage($_POST['to'],$_POST['subject'],$_POST['message'])) {
   
      echo "Message successfully sent!";
+ $url = 'https://api.sendgrid.com/';
+ $user = 'azure_d340c6977566f9e81343beb2c8a64e11@azure.com';
+ $pass = 'bLIZOvxl31me82y'; 
 
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      'to' => 'eddie81020@gmail.com',
+      'subject' => 'testing from curl',
+      'html' => 'testing body',
+      'text' => 'testing body',
+      'from' => 'lolteambuilder@hotmail.com',
+   );
+echo "STEP1";
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
 
    } else {
      // Tell user something went wrong it the return was false
