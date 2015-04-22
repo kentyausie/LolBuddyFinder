8ce8b3bf3087091a8b506169971f8d6e0bf49c21
Kent <jediknight101@gmail.com>
2015-04-21 20:18:49 -0600

    Changes

--- profile.php
+++ profile.php
@@ -33,7 +33,6 @@ $upRegion = strtoupper($row['region']);
 $row['language'] = ucfirst($row['language']);
 
 ?>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 	var leagueCheck="";
 	var region = "<?php echo $row['region']; ?>";
