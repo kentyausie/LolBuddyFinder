a742c6f171702c1c3882f0370d7c6532c4b0a983
eddie81020 <eddie81020@gmail.com>
2015-04-22 00:09:41 -0600

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,10 @@ $password = "awp0t6pokb";
 $dbname = "cmpt350_key053";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
+mysql_connect("lovett.usask.ca,cmpt350_key053,awp0t6pokb) or die("Error: ".mysql_error()); // Connection to the server
+13.
+mysql_select_db(cmpt350_key053) or die("Error: ".mysql_error()); // Connecting to the database
 
 // Check connection
 if ($conn->connect_error) {
