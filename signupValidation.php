3f09ee9d9d7ca514c0cf1f74cf1e9e64fe51a35b
Kent <jediknight101@gmail.com>
2015-04-21 23:26:12 -0600

    Changes

--- signupValidation.php
+++ signupValidation.php
@@ -126,8 +126,8 @@ if(isset($_POST['submit'])){
 	}
 	$timezone = $_POST['timezone'];
 	$language = $_POST['language'];
-	$profileId= $_POST['sumInfo']['profileIconId'];
-	$tier=$_POST['sumLeague']['tier'];
+	$profileId= $_POST['sumInfo']->id;
+	$tier=$_POST['sumLeague']->tier;
 	if($valid=="true"){
 		
 		$sql = "INSERT INTO Registration (summonerID, summoner, email, password, roleTop, roleMid, roleJungle, roleADC, roleSupport, champ1, champ2, champ3, champ4, champ5, region, timezone,language, profileID, tier) VALUES ('".$id."','".$sumName."','".$email."','".$password1."','".$top."','".$mid."','".$jungle."','".$adc."','".$support."','".$champions[0]."','".$champions[1]."','".$champions[2]."','".$champions[3]."','".$champions[4]."','".$region."','".$timezone."','".$language."','".$profileId."','".$tier."')";
